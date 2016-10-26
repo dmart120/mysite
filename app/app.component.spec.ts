8cc148aa16feeeaa07f8a1b96aac1273a87bf9c3
Daniel <daniel.martinez@kronos.com>
2016-10-25 19:41:48 -0500

    first attempt at testing

---
+++ app/app.component.spec.ts
@@ -0,0 +1,29 @@
+// import { TestBed } from '@angular/testing';
+import { AppComponent } from './app.component';
+import {TestBed} from "@angular/core/testing/test_bed";
+
+describe('Testing main app component', function () {
+    // let app: AppComponent;
+    // let fixture: any;
+    // let comp: any;
+    //
+    // beforeEach(() => {
+    //     app = new AppComponent();
+    //
+    //     TestBed.configureTestingModule({
+    //         declarations: [AppComponent]
+    //     });
+    //
+    //     fixture = TestBed.createComponent(AppComponent);
+    //     comp = fixture.componentInstance;
+    // });
+    //
+    // it('Should instantiate the component' , () => {
+    //     expect(comp).not.toBeNull();
+    // });
+
+    describe('1st test', () => {
+        it('should be true', () => expect(true).toBe(true));
+    });
+
+});
--- app/app.module.ts
+++ app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 
 import { NavBarComponent } from './components/navbar/navbar.component';
 import { NavBarElementComponent } from './components/navbar/navbar-element/navbar-element.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [
         AppComponent,
         NavBarComponent,
-        NavBarElementComponent
+        NavBarElementComponent,
+        CarouselComponent
     ],
     bootstrap: [
         AppComponent
